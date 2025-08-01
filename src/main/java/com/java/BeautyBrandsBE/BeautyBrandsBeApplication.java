cb4efcf13e34f58ccabcbf37bce35e9a94c38a92
Jenish Michael <jenishmichael21@gmail.com>
2025-08-01 14:02:27 +0530

    Removed DotEnv

--- src/main/java/com/java/BeautyBrandsBE/BeautyBrandsBeApplication.java
+++ src/main/java/com/java/BeautyBrandsBE/BeautyBrandsBeApplication.java
@@ -1,6 +1,6 @@
 package com.java.BeautyBrandsBE;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BeautyBrandsBeApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//		Dotenv dotenv = Dotenv.load();
+//		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
 		SpringApplication.run(BeautyBrandsBeApplication.class, args);
 		System.out.println("Hello World!!!");
--- src/test/java/com/java/BeautyBrandsBE/BeautyBrandsBeApplicationTests.java
+++ src/test/java/com/java/BeautyBrandsBE/BeautyBrandsBeApplicationTests.java
@@ -8,13 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BeautyBrandsBeApplicationTests {
 
-	@BeforeAll
-	static void setupEnv() {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-	}
+//	@BeforeAll
+//	static void setupEnv() {
+//		Dotenv dotenv = Dotenv.load();
+//		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//	}
 	@Test
 	void contextLoads() {
 	}
